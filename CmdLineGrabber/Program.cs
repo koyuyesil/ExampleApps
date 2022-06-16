dfd5bff683f33747c538c1d47c6bc99d09abdad1
koyuyesil <orsan@anitr.net>
2022-06-17 02:52:28 +0300

    sniffer added

---
+++ CmdLineGrabber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdLineGrabber
+{
+    class Program
+    {
+        static string path;
+        static void Main(string[] args)
+        {
+            path = AppDomain.CurrentDomain.BaseDirectory + " " + "sniffer.txt";
+            //path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            StreamWriter sw;
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (sw = (StreamWriter)File.CreateText(path))
+                {
+                    sw.WriteLine(string.Join(" ", args));
+                }
+            }
+            else
+            {
+                using (sw = (StreamWriter)File.AppendText(path))
+
+                {
+                    sw.WriteLine(string.Join(" ", args));
+                }
+            }
+        }
+    }
+}
