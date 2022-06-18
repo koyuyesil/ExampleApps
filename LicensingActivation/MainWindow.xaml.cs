aa5dda61b50c8cf1678453923a8bdbbb01989c9f
Örsan AKCİYER <orsan@anitr.net>
2022-06-18 05:24:38 +0300

    async

--- LicensingActivation/MainWindow.xaml.cs
+++ LicensingActivation/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +27,7 @@ namespace LicensingActivation
         public MainWindow()
         {
             InitializeComponent();
+            var a=GetUrlContentLengthAsync();
             bool fileExist = System.IO.File.Exists("Lisans.xml");
             if (fileExist)
             {
@@ -50,23 +52,49 @@ namespace LicensingActivation
                                 .AssertValidLicense();
             foreach (var failure in validationFailures) 
             { 
-                MessageBox.Show(failure.GetType().Name + ": " + failure.Message + " - " + failure.HowToResolve,"License Error",MessageBoxButton.OK,MessageBoxImage.Error);
-              
-            }
+                MessageBox.Show(failure.GetType().Name + ": " + failure.Message + " - " + failure.HowToResolve,"License Error",MessageBoxButton.OK,MessageBoxImage.Error);  
+            };
             
             if (validationFailures.Any())
             {
-                MessageBox.Show("Licence OK", "Standard Licensing OK", MessageBoxButton.OK, MessageBoxImage.Hand);
+                MessageBox.Show("Licence Error", "Standard Licensing Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Dispatcher.Invoke(Application.Current.Shutdown);
             }
             else
             {
-                MessageBox.Show("Licence Error", "Standard Licensing Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                System.Windows.Application.Current.Shutdown();
-            }
+                MessageBox.Show("Licence OK", "Standard Licensing OK", MessageBoxButton.OK, MessageBoxImage.Hand);
+            };
 
 
                 
 
         }
+
+
+
+
+        public async Task<int> GetUrlContentLengthAsync()
+        {
+            var client = new HttpClient();
+
+            Task<string> getStringTask =
+                client.GetStringAsync("https://docs.microsoft.com/dotnet");
+
+            DoIndependentWork();
+
+            string contents = await getStringTask;
+
+            return contents.Length;
+        }
+
+        void DoIndependentWork()
+        {
+            Console.WriteLine("Working...");
+        }
+
+
+
+
+
     }
 }
