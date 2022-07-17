ad4634c08593bdfb537ad9f0d6a859a85f8e3afe
Örsan AKCİYER <orsan@live.com>
2022-07-17 23:39:17 +0300

    async

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -6,13 +7,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            Method();
+            AsyncAwaitMethod();
             Camera.ResimKaydet();
             Camera.VideoKaydet();
             Camera.VideoKaydiDurdur();
-  
             Console.ReadKey();
 
+        }
+
+         private static void Method()
+        {
+            Console.WriteLine("1");
+            Task.Delay(1000);
+            Console.WriteLine("2");
+            Task.Delay(1000);
+            Console.WriteLine("3");
+            Task.Delay(1000);
+        }
+        async private static void AsyncAwaitMethod()
+        {
+            Console.WriteLine("4");
+            await Task.Delay(1000);
+            Console.WriteLine("5");
+            await Task.Delay(1000);
+            Console.WriteLine("6");
+            await Task.Delay(1000);
         }
     }
 }
---
+++ UWPApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Boş Sayfa öğe şablonu https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x41f adresinde açıklanmaktadır
+
+namespace UWPApp
+{
+    /// <summary>
+    /// Kendi başına kullanılabilecek ya da bir Çerçeve içine taşınabilecek boş bir sayfa.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Console.Beep();
+            Method();
+            Console.Beep();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        async private void Method()
+        {
+            Console.WriteLine("Hello");
+            await Task.Delay(1000);
+            Console.WriteLine("Hello");
+            await Task.Delay(1000);
+            Console.WriteLine("Hello");
+            await Task.Delay(1000);
+        }
+    }
+}
