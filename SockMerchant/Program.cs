b1ade0d789eb3307dd2d5aa13950be4a500081f9
koyuyesil <orsan@anitr.net>
2022-07-18 01:03:31 +0300

    sockmerchant

---
+++ SockMerchant/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SockMerchant
+{
+    internal class Program
+    {
+        static int sockMerchant(int[] socksPile)
+        {
+            List<int> a = new List<int>() ;
+            var pairsFound = 0;
+            var sockColorHash = new Dictionary<int, int>();
+
+            foreach (var sock in socksPile)
+            {
+                if (sockColorHash.ContainsKey(sock))
+                {
+                    pairsFound++;
+                    sockColorHash.Remove(sock);
+                }
+                else
+                    sockColorHash.Add(sock, 1);
+            }
+            return pairsFound;
+        }
+        static void Main(string[] args)
+        {
+            
+            var ar_temp = Console.ReadLine().Split(' ');
+            var ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            var result = sockMerchant(ar);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
